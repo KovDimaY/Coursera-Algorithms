8ad7a3ad3a38ed092c76d7d44651842843dfa587
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2017-06-18 22:35:05 +0200

    The problem of the course solved (35 mins)

--- src/com/coursera/assignment/SumCounter.java
+++ src/com/coursera/assignment/SumCounter.java
@@ -1,4 +1,5 @@
 package com.coursera.assignment;
+import java.util.Hashtable;
 
 import java.util.Arrays;
 
@@ -8,19 +9,32 @@ public class SumCounter {
 	
 	public int numberSums(long[] data, int[] interval) {
 		//System.out.println(Arrays.toString(data) + "\n" + Arrays.toString(interval) + "\n\n");
+		Hashtable hashtable = new Hashtable(2000000);
+		
+		System.out.println(data.length);
+		
+		
+		for (int i = 0; i < data.length; i++) {
+			hashtable.put(data[i], true);
+		}
+		
 		
 		int result = 0;
+		System.out.println(hashtable.size());
+		
+		
 		for(int t = interval[0]; t <= interval[1]; t++) {
-			// System.out.println("Checking for " + t);
+			System.out.println("Checking for " + t);
 			boolean discovered = false;
 			for(int j = 0; j < data.length && !discovered; j++) {
-				if (this.has(data, t - data[j]) && (t != 2*data[j])) {
+				if (hashtable.containsKey(t - data[j]) && (t != 2*data[j])) {
 					//System.out.println(data[j] + "+ ? = " + t);
 					result++;
 					discovered = true;
 				}
 			}
 		}
+		
 		
 		return result;
 	}
